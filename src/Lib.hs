46436f15c25e45d81f7552ea4f2335ff9e53c953
John Cowie <jcowie@thoughtworks.com>
2015-10-25 17:48:29 +0000

    John | encapsulate World constructor

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,30 +1,38 @@
-module Lib 
+module Lib
 (game) where
 
 import Data.Map as Map
 
-import MaybeHero.World
+import qualified MaybeHero.World as World
 import qualified MaybeHero.Input as Input
 import qualified MaybeHero.Command as Command
 import MaybeHero.Room
 import MaybeHero.Rooms
 
-world = World drawingRoom (describeRoom drawingRoom)
+world = World.mkWorld drawingRoom (describeRoom drawingRoom)
 game = doGame world
 
-doGame :: World -> IO ()
+doGame :: World.World -> IO ()
 doGame world = do
-  putStrLn . (++ "\n") . nextLine $ world
+  putStrLn . (++ "\n") . World.nextLine $ world
   line <- getLine
   case line of
     "exit" -> return ()
     _      -> doGame $ gameLogic line world
 
-gameLogic :: String -> World -> World
+gameLogic :: String -> World.World -> World.World
 gameLogic input oldWorld =
   case (Input.preProcess $ words input) of
+<<<<<<< HEAD
       [] -> updateLine oldWorld "..."
       (x:xs) -> case (Input.wordToCommand x) of
                   (Just cmd) -> cmd xs oldWorld
                   Nothing    -> updateLine oldWorld $ "I don't know how to " ++ input
 
+=======
+      [] -> World.updateLine oldWorld "..."
+      (x:xs) | x `elem` ["move","go","walk","run"] -> move xs oldWorld
+      (x:xs) | x == "help" -> help xs oldWorld
+      (x:xs) | x == "look" -> look xs oldWorld
+      _ -> World.updateLine oldWorld $ "I don't know how to " ++ input
+>>>>>>> John | encapsulate World constructor
--- src/MaybeHero/World.hs
+++ src/MaybeHero/World.hs
@@ -1,15 +1,27 @@
 module MaybeHero.World
-( World(..)
+( World
+, mkWorld
 , moveRoom
+, currentRoom
 , updateLine
+, nextLine
 ) where
 
 import qualified MaybeHero.Room as Room
 
-data World = World { room :: Room.Room, nextLine :: String }
+data World = World Room.Room String
 
 moveRoom :: World -> Room.Room -> World
 moveRoom (World _ n) r = World r (Room.describeRoom r)
+
+currentRoom :: World -> Room.Room
+currentRoom (World room _) = room
+
+nextLine :: World -> String
+nextLine (World _ n) = n
+
+mkWorld :: Room.Room -> String -> World
+mkWorld room nextLine = World room nextLine
 
 updateLine :: World -> String -> World
 updateLine (World room _) n = World room n
